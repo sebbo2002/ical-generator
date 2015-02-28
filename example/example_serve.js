d1ac6c5fc93765fef7e7a94d7c0fd112277e6bdd
Sebastian Pekarek <mail@sebbo.net>
2015-02-28 19:28:49 +0100

    example update

--- example/example_serve.js
+++ example/example_serve.js
@@ -1,8 +1,10 @@
-var ical = require('../lib/index.js'),
+'use strict';
+
+var ical = require('../lib/'),
 	http = require('http'),
 	cal = ical();
 
-cal.addEvent({
+cal.createEvent({
 	start: new Date(),
 	end: new Date(new Date().getTime() + 3600000),
 	summary: 'Example Event',
@@ -12,5 +14,6 @@ cal.addEvent({
 
 http.createServer(function(req, res) {
 	cal.serve(res);
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+}).listen(3000, '127.0.0.1', function() {
+	console.log('Server running at http://127.0.0.1:3000/');
+});
