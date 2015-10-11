b94bfdcdfd070fd6e101c5c21f4ad9654d8d3224
Sebastian <mail@sebbo.net>
2015-10-11 13:10:33 +0200

    Code Style update

--- gruntfile.js
+++ gruntfile.js
@@ -1,31 +1,31 @@
 module.exports = function(grunt) {
-	'use strict';
+    'use strict';
 
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.initConfig({
-		clean: {
-			coverage: ['coverage/**']
-		},
-		jshint: {
-			options: {
-				jshintrc: true
-			},
-			lib: [
-				'lib/*.js',
-				'test/*.js'
-			]
-		},
-		watch: {
-			lib: {
-				files: ['lib/*.js', 'test/*.js'],
-				tasks: ['jshint:lib']
-			}
-		}
-	});
+    grunt.initConfig({
+        clean: {
+            coverage: ['coverage/**']
+        },
+        jshint: {
+            options: {
+                jshintrc: true
+            },
+            lib: [
+                'lib/*.js',
+                'test/*.js'
+            ]
+        },
+        watch: {
+            lib: {
+                files: ['lib/*.js', 'test/*.js'],
+                tasks: ['jshint:lib']
+            }
+        }
+    });
 
-	grunt.registerTask('default', ['jshint:lib']);
-	grunt.registerTask('dev', ['jshint:lib', 'watch']);
+    grunt.registerTask('default', ['jshint:lib']);
+    grunt.registerTask('dev', ['jshint:lib', 'watch']);
 };
