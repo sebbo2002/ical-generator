fe7bf913ee2152d24cf6621383b71cb0ccb2fde2
[1000i100] Millicent Billette <git@1000i100.fr>
2018-09-30 04:37:02 +0200

    update built dist for personal use since merge. (DO NOT MERGE THIS)

--- dist/_tools.js
+++ dist/_tools.js
@@ -57,7 +57,7 @@ var ICalTools = function () {
     }, {
         key: 'escape',
         value: function escape(str) {
-            return str.replace(/[\\;,"]/g, function (match) {
+            return String(str).replace(/[\\;,"]/g, function (match) {
                 return '\\' + match;
             }).replace(/(?:\r\n|\r|\n)/g, '\\n');
         }
