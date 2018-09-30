27c293831645b85455251fafc42056ed34d99a09
[1000i100] Millicent Billette <git@1000i100.fr>
2018-09-30 04:24:20 +0200

    GEO: is include in generated .ics file with no escaped semicolon

--- src/event.js
+++ src/event.js
@@ -997,7 +997,7 @@ class ICalEvent {
 
 	    // GEO
 	    if (this._data.geo) {
-		    g += 'GEO:' + ICalTools.escape(this._data.geo.lat+';'+this._data.geo.lon) + '\r\n';
+		    g += 'GEO:' + ICalTools.escape(this._data.geo.lat)+';'+ICalTools.escape(this._data.geo.lon) + '\r\n';
 	    }
 
         // DESCRIPTION
