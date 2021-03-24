76d089729619c41e059446329c8e0d70445d533a
Sebastian Pekarek <mail@sebbo.net>
2021-03-24 20:16:55 +0100

    test(Index): "test" calendar exports

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,13 @@
 'use strict';
 
 import assert from 'assert';
-import ical from '../src';
-import ICalCalendar from '../src/calendar';
+import ical, {
+    ICalCalendar,
+    ICalCalendarMethod,
+    ICalEvent,
+    checkDate,
+    formatDate
+} from '../src';
 
 describe('ical-generator Index', function() {
     it('should be a function', function() {
@@ -11,5 +16,25 @@ describe('ical-generator Index', function() {
 
     it('should return a ICalCalendar', function() {
         assert.ok(ical() instanceof ICalCalendar);
+    });
+
+    it('should export ICalCalendar', function() {
+        assert.ok(ICalCalendar);
+    });
+
+    it('should export ICalCalendarMethod', function() {
+        assert.ok(ICalCalendarMethod);
+    });
+
+    it('should export ICalEvent', function() {
+        assert.ok(ICalEvent);
+    });
+
+    it('should export checkDate', function() {
+        assert.ok(checkDate);
+    });
+
+    it('should export formatDate', function() {
+        assert.ok(formatDate);
     });
 });
