01bf75a42dca7819c65fbdded027b06437260601
Sebastian Pekarek <mail@sebbo.net>
2021-07-16 20:49:08 +0200

    chore(repo-template): Fix docker release step

--- release.config.js
+++ release.config.js
@@ -10,69 +10,60 @@ const configuration = {
     'plugins': []
 };
 
-configuration.push(['@semantic-release/commit-analyzer', {
+configuration.plugins.push(['@semantic-release/commit-analyzer', {
     'releaseRules': [
         {'type': 'build', 'scope': 'deps', 'release': 'patch'},
         {'type': 'docs', 'release': 'patch'}
     ]
 }]);
 
-configuration.push('@semantic-release/release-notes-generator');
+configuration.plugins.push('@semantic-release/release-notes-generator');
 
-configuration.push(['@semantic-release/exec', {
+configuration.plugins.push(['@semantic-release/exec', {
     'prepareCmd': './.github/workflows/build.sh'
 }]);
 
-configuration.push('@semantic-release/changelog');
+configuration.plugins.push('@semantic-release/changelog');
 
-configuration.push('semantic-release-license');
+configuration.plugins.push('semantic-release-license');
 
-configuration.push(['@amanda-mitchell/semantic-release-npm-multiple', {
+configuration.plugins.push(['@amanda-mitchell/semantic-release-npm-multiple', {
     'registries': {
         'github': {},
         'public': {}
     }
 }]);
 
-configuration.push(['@semantic-release/github', {
+configuration.plugins.push(['@semantic-release/github', {
     'labels': false,
     'assignees': process.env.GH_OWNER
 }]);
 
-configuration.push(['@semantic-release/git', {
+configuration.plugins.push(['@semantic-release/git', {
     'assets': ['CHANGELOG.md', 'LICENSE'],
     'message': 'chore(release): :bookmark: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
 }]);
 
-if(process.env.DOCKER_LOCAL_IMAGE_DH) {
-    configuration.push(['@eclass/semantic-release-docker', {
-        'baseImageName': process.env.DOCKER_LOCAL_IMAGE_DH,
-        'registries': [
-            {
-                'url': 'docker.io',
-                'imageName': 'docker.io/' + process.env.DOCKER_REGISTRY_IMAGE,
-                'user': 'DOCKER_REGISTRY_USERNAME',
-                'password': 'DOCKER_REGISTRY_TOKEN'
-            }
-        ]
+if (process.env.DOCKER_LOCAL_IMAGE_DH) {
+    configuration.plugins.push(['@semantic-release-plus/docker', {
+        name: process.env.DOCKER_LOCAL_IMAGE_DH,
+        publishLatestTag: true,
+        publishMajorTag: true,
+        publishMinorTag: true
     }]);
 }
 
-if(process.env.DOCKER_LOCAL_IMAGE_GH) {
-    configuration.push(['@eclass/semantic-release-docker', {
-        'baseImageName': 'ghcr.io/' + process.env.DOCKER_LOCAL_IMAGE_GH,
-        'registries': [
-            {
-                'url': 'ghcr.io',
-                'imageName': 'ghcr.io/' + process.env.GH_REPO,
-                'user': 'GITHUB_REGISTRY_USERNAME',
-                'password': 'GITHUB_REGISTRY_TOKEN'
-            }
-        ]
+if (process.env.DOCKER_LOCAL_IMAGE_GH) {
+    configuration.plugins.push(['@semantic-release-plus/docker', {
+        name: process.env.DOCKER_LOCAL_IMAGE_GH,
+        registryUrl: 'ghcr.io',
+        publishLatestTag: true,
+        publishMajorTag: true,
+        publishMinorTag: true
     }]);
 }
 
-configuration.push(['@qiwi/semantic-release-gh-pages-plugin', {
+configuration.plugins.push(['@qiwi/semantic-release-gh-pages-plugin', {
     'msg': 'docs: Updated for <%= nextRelease.gitTag %>',
     'src': './docs',
     'dst': `./${process.env.BRANCH}`,
