7b86a4f1187c387a3a5792e1fb72d822b04e3631
Sebastian Pekarek <mail@sebbo.net>
2022-06-28 20:38:18 +0200

    build: Native ESM support
    BREAKING CHANGE: Only Support for node.js ^12.20.0 || >=14.13.1

--- test/lib/index.ts
+++ test/lib/index.ts
@@ -1,7 +1,7 @@
 'use strict';
 
-import Magic from '../../src/lib';
-import assert = require('assert');
+import Magic from '../../src/lib/index.js';
+import assert from 'assert';
 
 describe('Example', function () {
     it('should work with integers', function () {
