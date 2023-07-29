30fcccdfb1253c37f9211833a6d74ecbf953b892
Wiktor Plaga <wiktor.plaga@tzif.io>
2023-07-29 18:48:33 +0200

    fix: double-quotes and missing filename variable

--- examples/example-nextjs13.js
+++ examples/example-nextjs13.js
@@ -1,39 +1,41 @@
 // app/api/calendar/route.ts
 
-import icalendar from 'ical-generator'
-import moment from 'moment'
+import icalendar from "ical-generator"
+import moment from "moment"
 
 export async function GET(req) {
-  if (req.method !== 'GET') {
-    return new Response('Method Not Allowed', {
-        headers: { Allow: 'GET' },
+    if (req.method !== "GET") {
+    return new Response("Method Not Allowed", {
+        headers: { Allow: "GET" },
         status: 405,
     })
-  }
+    }
 
-  try {
+    const filename = "calendar.ics"
+
+    try {
     const calendar = icalendar({
-        prodId: '//superman-industries.com//ical-generator//EN',
+        prodId: "//superman-industries.com//ical-generator//EN",
         events: [
             {
                 start: moment(),
-                end: moment().add(1, 'hour'),
-                summary: 'Example Event',
-                description: 'It works ;)',
-                url: 'https://example.com'
+                end: moment().add(1, "hour"),
+                summary: "Example Event",
+                description: "It works ;)",
+                url: "https://example.com"
             }
         ]
     });
 
     return new Response(calendar.toString(), {
         headers: {
-            "Content-Type": 'text/calendar; charset=utf-8',
+            "Content-Type": "text/calendar; charset=utf-8",
             "Content-Disposition": `attachment; filename="${filename}"`,
         },
         status: 200,
     })
-  } catch (err) {
+    } catch (err) {
     console.error(err)
     return new Response(JSON.stringify(err), { status: 500 })
-  }
+    }
 }
